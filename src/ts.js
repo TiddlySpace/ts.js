c279c91ea2d7d3928e0dfcddca60afc20726e5ea
Jon Robson <jdlrobson@gmail.com>
2011-07-15 12:02:13 +0100

    took away csrf dependency
    currently https://github.com/TiddlySpace/tiddlyspace/blob/master/src/plugins/TiddlySpaceCSRF.js
    is a dependency.
    
    clearing that up here.

--- src/ts.js
+++ src/ts.js
@@ -1,4 +1,22 @@
 // version 0.1.2dev
+(function() {
+var getCSRFToken = function(window) {
+	// XXX: should not use RegEx - cf.
+	// http://www.quirksmode.org/js/cookies.html
+	// https://github.com/TiddlySpace/tiddlyspace/commit/5f4adbe009ed4bda3ce39058a3fb07de1420358d
+	var regex = /^(?:.*; )?csrf_token=([^(;|$)]*)(?:;|$)/;
+	var match = regex.exec(document.cookie);
+	var csrf_token = null;
+	if (match && (match.length === 2)) {
+		csrf_token = match[1];
+	}
+
+	return csrf_token;
+};
+
+window.getCSRFToken = getCSRFToken;
+})(window);
+
 var ts = {
 	locale: {
 		error: "An error occurred",
