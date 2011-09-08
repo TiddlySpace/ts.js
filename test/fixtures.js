29239be9c1f8527d434f703ea9cc234f06e47e28
Jon Robson <jdlrobson@gmail.com>
2011-09-08 14:54:40 +0100

    update fixtures for new test
    the new test which passes in the space foo will now trigger a request
    for the members of a space, this prevents it from throwing an
    exception

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,3 +1,7 @@
+var xhr = {
+	getResponseHeader: function() {}
+};
+
 var _oldAjax = $.ajax;
 $.ajax = function(options) {
 	var handlers = {
@@ -5,6 +9,9 @@ $.ajax = function(options) {
 			"/status": function(options) {
 				options.success({"username": "jon", "challengers": [], 
 					"server_host": {"host": "tiddlyspace.com", "scheme": "http", "port": "80"}});
+			},
+			"http://foo.tiddlyspace.com/spaces/foo/members": function(options) {
+				options.success(["jon"], "200", xhr);
 			}
 		}
 	};
