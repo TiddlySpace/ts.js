9dc047c770d0b388014c380a50d40a98f272e65f
Ben Gillies <bengillies@gmail.com>
2012-07-11 17:49:52 +0100

    Fixed the ordering of error messages on registering

--- src/ts.js
+++ src/ts.js
@@ -503,8 +503,9 @@ var ts = {
 						password === passwordConfirm) {
 					ts.register(username, password, ev.target, options);
 				} else {
-					var msg = validName ? ts.locale.passwordError :
-						validLength ? ts.locale.passwordLengthError :
+					var msg = validName ? (validLength ?
+						ts.locale.passwordLengthError :
+						ts.locale.passwordError) :
 						ts.locale.charError;
 					options.annotate = validName ? "[type=password]" : "[name=username]";
 					ts.messages.display(form, msg, true, options);
